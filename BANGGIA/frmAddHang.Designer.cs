52514b6d0304338c8bb697041cf31555113fbd05
luongchung <chunglv42@wru.vn>
2018-07-19 20:38:18 +0700

    first commit

---
+++ BANGGIA/frmAddHang.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace banggia
+{
+    partial class frmAddHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAddHang));
+            this.txtTenHang = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lueLoaiHang = new DevExpress.XtraEditors.LookUpEdit();
+            this.btnLuu = new DevExpress.XtraEditors.SimpleButton();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.lueLoaiHang.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTenHang
+            // 
+            this.txtTenHang.Location = new System.Drawing.Point(142, 74);
+            this.txtTenHang.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTenHang.Name = "txtTenHang";
+            this.txtTenHang.Size = new System.Drawing.Size(325, 27);
+            this.txtTenHang.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 77);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tên hàng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 119);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Loại hàng";
+            // 
+            // lueLoaiHang
+            // 
+            this.lueLoaiHang.Location = new System.Drawing.Point(142, 116);
+            this.lueLoaiHang.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lueLoaiHang.Name = "lueLoaiHang";
+            this.lueLoaiHang.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.lueLoaiHang.Properties.DisplayMember = "TenLoai";
+            this.lueLoaiHang.Properties.NullText = "[Mời chọn loại hàng]";
+            this.lueLoaiHang.Properties.ValueMember = "ID";
+            this.lueLoaiHang.Size = new System.Drawing.Size(324, 26);
+            this.lueLoaiHang.TabIndex = 7;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnLuu.ImageOptions.Image")));
+            this.btnLuu.Location = new System.Drawing.Point(142, 327);
+            this.btnLuu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(324, 53);
+            this.btnLuu.TabIndex = 6;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(143, 168);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(324, 27);
+            this.txt1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 171);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 19);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "O1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 211);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 19);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "O2";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(143, 208);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(324, 27);
+            this.txt2.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(92, 247);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 19);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "O3";
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(142, 244);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(324, 27);
+            this.txt3.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(92, 285);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 19);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "O4";
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(142, 282);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(324, 27);
+            this.txt4.TabIndex = 15;
+            // 
+            // frmAddHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 456);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lueLoaiHang);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTenHang);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmAddHang";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thêm hàng";
+            this.Load += new System.EventHandler(this.frmAddHang_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.lueLoaiHang.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTenHang;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.SimpleButton btnLuu;
+        private System.Windows.Forms.Label label4;
+        private DevExpress.XtraEditors.LookUpEdit lueLoaiHang;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt4;
+    }
+}
---
+++ BANGGIA/frmAddLoaiHang.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace banggia
+{
+    partial class frmAddLoaiHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAddLoaiHang));
+            this.txtTenLoai = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnThem = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // txtTenLoai
+            // 
+            this.txtTenLoai.Location = new System.Drawing.Point(216, 83);
+            this.txtTenLoai.Name = "txtTenLoai";
+            this.txtTenLoai.Size = new System.Drawing.Size(341, 33);
+            this.txtTenLoai.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tên loại";
+            // 
+            // btnThem
+            // 
+            this.btnThem.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnThem.ImageOptions.Image")));
+            this.btnThem.Location = new System.Drawing.Point(216, 145);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(341, 57);
+            this.btnThem.TabIndex = 2;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // frmAddLoaiHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 311);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTenLoai);
+            this.Name = "frmAddLoaiHang";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thêm loại hàng";
+            this.Load += new System.EventHandler(this.frmAddLoaiHang_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTenLoai;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.SimpleButton btnThem;
+    }
+}
